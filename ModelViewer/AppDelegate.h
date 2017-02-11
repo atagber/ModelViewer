ac8529dfe021685fa3ed05c842273129515f54e1
Arman <arman@iMac-Bradley-2.local>
2017-02-11 16:11:01 +0300

    Initial commit

---
+++ ModelViewer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ModelViewer
+//
+//  Created by Arman on 06.02.17.
+//  Copyright © 2017 Arman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ModelViewer/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  ModelViewer
+//
+//  Created by Arman on 06.02.17.
+//  Copyright © 2017 Arman. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+@end
